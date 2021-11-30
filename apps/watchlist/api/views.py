1e3a824d40017dfb82aadbdc11ca416db9d58a76
kofi-teddy <agudogot@gmail.com>
2021-11-30 13:02:29 +0000

    movies api post and get request updated

--- apps/watchlist/api/views.py
+++ apps/watchlist/api/views.py
@@ -5,12 +5,21 @@ from apps.watchlist.api.serializers import MovieSerializer
 from apps.watchlist.models import Movie
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
 
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 @api_view()
 def movie_detail(request, pk):
