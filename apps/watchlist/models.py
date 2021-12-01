3c583b39dbbc37cb5c623b8b4c2ceecfbdc3d69d
kofi-teddy <agudogot@gmail.com>
2021-12-01 15:17:41 +0000

    review model

--- apps/watchlist/models.py
+++ apps/watchlist/models.py
@@ -31,4 +31,4 @@ class Review(models.Model):
     update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.rating
+        return f'{str(self.rating)} ⭐ {self.watchlist.title}'
