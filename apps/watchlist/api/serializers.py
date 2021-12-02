2d468ccec16c245e9764dd3935db4661301e0cca
kofi-teddy <agudogot@gmail.com>
2021-12-02 05:17:43 +0000

    updated review serializer with review_user field

--- apps/watchlist/api/serializers.py
+++ apps/watchlist/api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Review
