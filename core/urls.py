3d7f641adb76e3f72bebcf062da0dd00e80161e9
kofi-teddy <agudogot@gmail.com>
2022-02-05 23:54:42 +0000

    added puppies api urls to core urls

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('apps.watchlist.api.urls')),
+    path('', include('puppies.urls')),
+    path('api-auth/', namespace='rest_framework'),
 ]
