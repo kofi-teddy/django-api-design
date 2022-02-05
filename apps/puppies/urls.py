6ebefb0a4b3bd60af03f58776947e73f72fcdda2
kofi-teddy <agudogot@gmail.com>
2022-02-05 23:55:42 +0000

    fix puppies urls

--- apps/puppies/urls.py
+++ apps/puppies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.puppies import views
 
 urlpatterns = [
-    path('api/v1/puppies/<pk>', views.get_delete_update_puppy, name='get_delete_update_puppy'),
+    path('api/v1/puppies/<pk>/', views.get_delete_update_puppy, name='get_delete_update_puppy'),
     path('api/v1/puppies/', views.get_post_puppies, name='get_post_puppies'),
 ]
