8cc76cfb88ac90e02f387d01d9b80de7141de34f
kofi-teddy <agudogot@gmail.com>
2022-02-06 01:12:11 +0000

    test create valid puppy and invalid puppy

---
+++ apps/puppies/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from puppies.models import Puppy
+
+
+class PuppyTest(TestCase):
+    '''
+    Test module for Puppy model
+    '''
+
+    def setUp(self):
+        Puppy.objects.create(name='Ama', age=24, breed='Bull Dog', color='Black')
+        Puppy.objects.create(name='Akos', age=24, breed='Gradane', color='Brown')
+
+    def test_puppy_breed(self):
+        puppy_ama = Puppy.objects.get(name='Ama')
+        puppy_akos = Puppy.objects.get(name='Akos')
+        self.assertEqual(puppy_ama.get_breed(), 'Ama belongs to Bull Dog breed.')
+        self.assertEqual(puppy_akos.get_breed(), 'Akos belongs to Gradane breed.')
--- tests/test_views.py
+++ tests/test_views.py
@@ -64,3 +64,39 @@ class GetSinglePuppyTest(TestCase):
             reverse('get_delete_update_puppy', kwargs={'pk': 30})
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    
+class CreateNewPuppyTest(TestCase):
+    '''
+    Test module for inserting a new puppy.
+    '''
+
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Ama',
+            'age': 24,
+            'breed': 'Pamerion',
+            'color': 'White'
+        }
+        self.valid_payload = {
+            'name': '',
+            'age': 24,
+            'breed': 'Pamerion',
+            'color': 'White'
+        }
+
+    def test_create_valid_puppy(self):
+        response = client.post(
+           reverse('get_post_puppies'),
+           data=json.dumps(self.valid_payload),
+           content_type='application/json' 
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_create_invalid_puppy(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
